39bc061faba924e192ce6a6749e25efdf2a6b134
apcarter05 <35357323+apcarter05@users.noreply.github.com>
2018-03-07 17:10:12 -0700

    windchill

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,6 +1,12 @@
-function windChill() {
-    var t = average temp;
-    var s = wind speed MPH;
-    var f = wind chill F;
-    f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16;
-}
+var high = parseFloat(document.getElementById('highT').innerHTML);
+var low = parseFloat(document.getElementById('lowT').innerHTML);
+var averageT = (high + low) / 2;
+var windspeed = parseFloat(document.getElementById('speed').innerHTML);
+
+var w = Math.pow(windspeed,0.16);
+
+var windchill =  35.74 + (0.6215 * averageT) - (35.75 * w) + (0.4275 * averageT * w);
+
+windchill = windchill.toFixed(2);
+
+document.getElementById('wind-chill').innerHTML = windchill;
