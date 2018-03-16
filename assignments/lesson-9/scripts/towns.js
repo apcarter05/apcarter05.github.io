fe8b60f6e20db2492022516c76e8e28dbe8aa63d
apcarter05 <35357323+apcarter05@users.noreply.github.com>
2018-03-16 01:20:39 -0600

    index

---
+++ assignments/lesson-9/scripts/towns.js
@@ -0,0 +1,40 @@
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var townData = request.response;
+    populateTownData(townData);
+}
+
+
+function populateTownData(jsonObj) {
+    var towns = jsonObj['towns'];
+    
+    for (var i = 0; i < towns.length; i++) {
+        if (towns[i].name == "Franklin" || towns[i].name == "Springfield" || towns[i].name == "Greenville") {
+            var myArticle = document.getElementById(towns[i].name);
+            var myH2 = document.createElement('h2');
+            var myPara1 = document.createElement('p');
+            var myPara2 = document.createElement('p');
+            var myPara3 = document.createElement('p');
+            var myPara4 = document.createElement('p');
+            var myList = document.createElement('ul');
+        
+            myH2.textContent = towns[i].name;
+            myPara1.textContent = towns[i].motto;
+            myPara2.textContent = towns[i].yearFounded;
+            myPara3.textContent = towns[i].currentPopulation;
+            myPara4.text.Content = towns[i].averageRainfall;
+
+            myArticle.appendChild(myH2);
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);
+            myArticle.appendChild(myList);
+        }
+    }
+     
+}
